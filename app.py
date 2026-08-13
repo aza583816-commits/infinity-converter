a4480989ca2e0dbdec6c2fd70cc8641fd65c9528
aza583816-commits <aza583816@gmail.com>
2026-08-13 22:58:54 +0300

    Update app.py

--- app.py
+++ app.py
@@ -537,5 +537,7 @@ def convert():
         app.logger.exception("convert() error")
         return jsonify({"error": str(exc)}), 500
 
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000, debug=True)
+if __name__ == '__main__':
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
+
