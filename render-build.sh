b639d7be55f8de0ed139c68f526d20f5664466b6
aza583816-commits <aza583816@gmail.com>
2026-08-20 21:03:18 +0300

    Update render-build.sh

--- render-build.sh
+++ render-build.sh
@@ -3,7 +3,8 @@
 set -o errexit
 
 # تحديث وتثبيت LibreOffice والخطوط لدعم اللغة العربية بالكامل
-apt-get update && apt-get install -y libreoffice libreoffice-writer fonts-noto-core fonts-noto-extra
+apt-get update && apt-get install -y libreoffice libreoffice-writer fonts-noto-core fonts-noto-extra tesseract-ocr tesseract-ocr-ara tesseract-ocr-eng
+
 
 # تثبيت مكتبات بايثون كالمعتاد
 pip install -r requirements.txt
